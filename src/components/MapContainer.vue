3feed3a07864a596e3ae437bc2b59c02a077f383
hugh <820498024@qq.com>
2023-06-08 17:46:56 +0800

    提交

--- src/components/MapContainer.vue
+++ src/components/MapContainer.vue
@@ -6,8 +6,6 @@ import * as Cesium from "cesium";
 import { Viewer } from "cesium";
 import { onMounted } from "vue";
 window.Cesium = Cesium;
-Cesium.Ion.defaultAccessToken =
-	"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI4ODE2MTJmNi02NjA4LTRmOWYtODhiNi1jYjZhYWQ0NTZjZWYiLCJpZCI6ODYzODEsImlhdCI6MTY0Nzc3NTE0OX0.RM0iXXF2qCSAiqfLBRY2siP-gSMGgfHxQTMRX6WSH2A";
 // 设置Cesium相机默认位置在中国区域上方
 Cesium.Camera.DEFAULT_VIEW_RECTANGLE = Cesium.Rectangle.fromDegrees(90, -20, 110, 90);
 onMounted(() => {
